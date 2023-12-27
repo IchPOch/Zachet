2ccb9e5250c504671aa57ea28d2df7a032ec145b
Eprintsev <User@WIN-U9BQSB2CRNG>
2023-12-27 13:52:52 +0300

    Добавьте файлы проекта.

---
+++ zachrt/zachrt.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <numeric>
+
+constexpr int N = 1000000;
+constexpr int NumThreads = 4;
+
+
+auto partialSum = [](int start, int end) {
+    double result = 0;
+    for (int n = start; n <= end; ++n) {
+        result += static_cast<double>(n + 1) / n;
+    }
+    return result;
+};
+
+int main() {
+    double sum = 0;
+
+    std::vector<std::thread> threads;
+
+    int step = N / NumThreads;
+
+    for (int i = 0; i < NumThreads; ++i) {
+        int start = i * step + 1;
+        int end = (i == NumThreads - 1) ? N : (i + 1) * step;
+        threads.emplace_back([start, end, &sum, i]() {
+            sum += partialSum(start, end);
+            });
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    std::cout << "Total sum: " << sum << std::endl;
+
+    return 0;
+}
